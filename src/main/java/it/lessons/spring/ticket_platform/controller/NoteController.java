133a0c35120fbabf262f231d333acbf7e07a63b5
IlTuoNome <difrancesco.davide93@gmail.com>
2025-05-16 14:35:06 +0200

    fix: add Note

---
+++ src/main/java/it/lessons/spring/ticket_platform/controller/NoteController.java
@@ -0,0 +1,67 @@
+package it.lessons.spring.ticket_platform.controller;
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import it.lessons.spring.ticket_platform.model.Note;
+import it.lessons.spring.ticket_platform.repository.NoteRepository;
+import it.lessons.spring.ticket_platform.repository.TicketRepository;
+import jakarta.validation.Valid;
+
+
+
+@Controller
+@RequestMapping("/ticket")
+public class NoteController {
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+    @Autowired
+    private TicketRepository ticketRepository;
+    
+    /*Creazione Note*/
+    @GetMapping("/{id}/note")
+    public String addNote(@PathVariable Integer id, Model model){
+        Note addNote = new Note();
+        addNote.setTicket(ticketRepository.findById(id).get());
+        
+        //Imposta la data automaticamente alla creazione
+        addNote.setNoteDate(LocalDate.now());
+
+        model.addAttribute("note", addNote);
+        return "note/create";
+    }
+    @PostMapping("/note")
+    public String newNote(@Valid @ModelAttribute("note") Note formNote, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes){
+        if (bindingResult.hasErrors()) {
+            
+            //todo togliere
+            System.out.println("Errori di binding:");
+            bindingResult.getAllErrors().forEach(e -> System.out.println(e));
+            //todo togliere
+
+            // Riaggancia il Ticket prima di ritornare alla view (se serve)
+            Integer id = formNote.getTicket().getId();
+            formNote.setTicket(ticketRepository.findById(id).orElse(null));
+            model.addAttribute("note", formNote);
+            return "note/create";  // Torna alla pagina con gli errori di validazione
+        }
+        //Imposta automaticamente la data odierna
+        formNote.setNoteDate(LocalDate.now());
+
+        noteRepository.save(formNote);
+        redirectAttributes.addFlashAttribute("successMessage", "Nota aggiunta");
+        return "redirect:/ticket/show/" + formNote.getTicket().getId();
+    }
+    
+}
